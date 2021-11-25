25613a2925751d285ccff2e0c7ce0dfc6135dfa3
kim-min-ju449 <77184157+kim-min-ju449@users.noreply.github.com>
2021-11-25 22:29:11 +0900

    완성!!

--- BI/main.py
+++ BI/main.py
@@ -1,6 +1,5 @@
 import tkinter
 from tkinter import *
-from tkinter.font import ITALIC
 from PyQt5.QtCore import QDateTime
 import datetime as dt
 from pygame.rect import *
@@ -9,8 +8,6 @@ import datetime
 import random
 import pygame
 global score
-li =[]
-#BASICFRONT = pygame.font.SysFont(ITALIC)
 def juice():
     global new
     new = Toplevel()
@@ -56,11 +53,7 @@ def btncall2():
     global new
     new = Toplevel()
     btn = Button(new, command=juice)
-    # img = PhotoImage(file="bananaleaf.png")
-    # photo = PhotoImage(file="m.gif")
-    # btn = Button(new, image=photo)
-    #btn.pack(expand=1, anchor=CENTER)
-    btn.config(width=5, height=2, bg="#9FF781", font=('koverwatch', 30),)  # 버튼 가로 세로 크기 변경
+    btn.config(width=5, height=2, bg="#FAAC58", font=('koverwatch', 30),)  # 버튼 가로 세로 크기 변경
     btn.config(text="주스")  # 현재 시각
     btn.pack(side=LEFT, padx=70)  # 버튼 배
     btn2 = Button(new, command=bread)
@@ -120,7 +113,7 @@ def btncall3():
     ddong_y_pos = 0
     ddong_speed = 10
 
-    #돈만들기
+    #바나나만들기
     don = pygame.image.load("banana.png")
     don_size = don.get_rect().size
     don_width = don_size[0]
@@ -201,6 +194,7 @@ def btncall3():
         screen.blit(background, (0, 0))
         screen.blit(character, (character_x_pos, character_y_pos))
         screen.blit(ddong, (ddong_x_pos, ddong_y_pos))
+        screen.blit(ddong, (ddong_x_pos, ddong_y_pos))
         screen.blit(don, (don_x_pos, don_y_pos))
         font_01 = pygame.font.SysFont("FixedSsy", 30, True, False)
         text_score = font_01.render("Score : " + str(score), True, BLACK)
@@ -215,23 +209,10 @@ def btncall3():
     with open('score.txt', 'a') as f:
         f.write(f'{score}\t{datetime.datetime.now()}\n')
 
-    # count+=1
-    # score+=1
-    # li.append(score)
-    # print(li)
-    # f = open('score.txt', 'w', encoding='utf-8')
-    # for i in range(0, count + 1):
-    #
-    #     f.write("점수"+str(score)+" "+str(today())+'\n')
-    #
-    #
-    # f.close()
-
 
 def data():
     f = open('score.txt', 'w', encoding='utf-8')
 
-   # f.write(score)
     f.write('\n')
     f.write('월드')
     f.write('\n')
@@ -241,34 +222,35 @@ def data():
 
     print('with')
 
-    #with open('text.txt', 'w', encoding='utf-8') as f:
 
-
-def btncall4():
-    global new
-    new = Toplevel()
-    new.geometry("800x600")
-    #datetime = QDateTime.currentDateTime()
-    x = dt.datetime.now()
-    x
-    label = tkinter.Label(new, text=x, width=50, height=5, fg="BLACK", relief="solid")
-    #label2 = tkinter.Label(new, text=score, width=50, height=5, fg="BLACK", relief="solid")
-    label.pack()
-    #label2.pack()
-
-    #new.config("text")
-    # canvas = Canvas(new, bg='Yellow')
-    # canvas.pack(expand=YES, fill=BOTH)
-    # img=PhotoImage(file="bananaI.png")
-    # canvas.create_image(10, 10, anchor=NW, image=img)
-    #print(str(score))
-    new.mainloop()
-
+# def btncall4():
+#     global new
+#     new = Toplevel()
+#     new.geometry("800x600")
+#     #datetime = QDateTime.currentDateTime()
+#     x = dt.datetime.now()
+#     x
+#     label = tkinter.Label(new, text=x, width=50, height=5, fg="BLACK", relief="solid")
+#     #label2 = tkinter.Label(new, text=score, width=50, height=5, fg="BLACK", relief="solid")
+#     label.pack()
+#     #label2.pack()
+#
+#     #new.config("text")
+#     # canvas = Canvas(new, bg='Yellow')
+#     # canvas.pack(expand=YES, fill=BOTH)
+#     # img=PhotoImage(file="bananaI.png")
+#     # canvas.create_image(10, 10, anchor=NW, image=img)+
+#     #print(str(score))
+#     new.mainloop()
+#
 
 def main():
     win = Tk()  # 창 생성
     win.geometry("1000x600")
-    win.title("연습")
+    win.title("BI")
+    win.configure(bg='white')
+    lbl = Label(win, text="BI" ,font = ("궁서체",70), bg='white')
+    lbl.pack(pady=50)
     # canvas = Canvas(win, bg='Yellow')
     # canvas.pack(expand=YES, fill=BOTH)
     # img = PhotoImage(file="bananaleaf.png")
@@ -286,10 +268,10 @@ def main():
     btn3.config(width=10, height=2, bg="#81BEF7", font=('koverwatch', 30), )  # 버튼 가로 세로 크기 변경
     btn3.config(text="게임하기")  # 현재 시각
     btn3.pack(side=LEFT, padx=40)  # 버튼 배
-    btn4 = Button(win, command=btncall4)
-    btn4.config(width=10, height=2, bg="#81BEF7", font=('koverwatch', 30), )  # 버튼 가로 세로 크기 변경
-    btn4.config(text="게임기록")  # 현재 시각
-    btn4.pack(side=LEFT, padx=40)  # 버튼 배
+    # btn4 = Button(win, command=btncall4)
+    # btn4.config(width=10, height=2, bg="#81BEF7", font=('koverwatch', 30), )  # 버튼 가로 세로 크기 변경
+    # btn4.config(text="게임기록")  # 현재 시각
+    # btn4.pack(side=LEFT, padx=40)  # 버튼 배
     # canvas = Canvas(win, bg='Yellow')
     # canvas.pack(expand=YES, fill=BOTH)
     win.mainloop()  # 창 실행
